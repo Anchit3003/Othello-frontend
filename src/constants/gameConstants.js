9fa427ce2133680c27cf825d0ce5054c1e41a7af
Anchit <anchitkanaujia.98@gmail.com>
2025-08-26 01:28:07 +0530

    crated the board using canvas

---
+++ src/constants/gameConstants.js
@@ -0,0 +1,5 @@
+export const BOARD_SIZE = 8;          
+export const CANVAS_SIZE = 620;     
+export const CELL_SIZE = CANVAS_SIZE / BOARD_SIZE;
+export const BOARD_COLOR = "#0f5132"; 
+export const GRID_COLOR = "white";   
---
+++ src/pages/Homes.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Grid from "../components/Grid";
+import "./Home.css";
+import othellooo from "../assets/othellooo.png";
+export default function Home() {
+  return (
+    <div className="home-container">
+    
+      <aside className="side-nav">
+      <div className="header">
+          <img src={othellooo} alt="Othello Logo" className="logo" />
+          <h2>Othello Game</h2>
+        </div>
+
+      </aside>
+      <div className="game-area">
+        <Grid />
+      </div>
+    </div>
+  );
+}
